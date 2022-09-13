0b7306e038cc84df235d2238d823393bbf240914
Wins <tktmmpaas@gmail.com>
2022-09-13 15:22:11 +0200

    ajout du nouveau mini-jeu 'LIRE'

--- controllers/ecrire.js
+++ controllers/ecrire.js
@@ -1,8 +1,10 @@
+import { options } from "../particles/particles-options.js";
+
 const baliseScript = document.querySelector(".script-particules");
 const imageAleatoire = document.querySelector(".image-aleatoire");
 const dynamicLetters = document.querySelector(".dynamic-letters");
-const btnJouerEcrire = document.querySelector(".btn-jouer-ecrire");
-const lettersButton = document.querySelectorAll(".img-btn-lettre");
+const btnJouerEcrire = document.querySelector(".btn-ecrire");
+const lettersButton = document.querySelectorAll(".img-btn");
 const reaction = document.querySelector(".reaction");
 const audio = document.querySelector(".audio1");
 // const audio2 = document.querySelector(".audio2");
@@ -145,126 +147,3 @@ lettersButton.forEach((imgBtn) => {
     verifieLettreCliquee(e);
   });
 });
-
-const options = {
-  fullScreen: {
-    zIndex: 1,
-  },
-  particles: {
-    number: {
-      value: 120,
-    },
-    color: {
-      value: ["#00FFFC", "#FC00FF", "#fffc00"],
-    },
-    shape: {
-      type: ["circle", "square"],
-      options: {},
-    },
-    opacity: {
-      value: 1,
-      animation: {
-        enable: true,
-        minimumValue: 0,
-        speed: 2,
-        startValue: "max",
-        destroy: "min",
-      },
-    },
-    size: {
-      value: 7,
-      random: {
-        enable: true,
-        minimumValue: 2,
-      },
-    },
-    links: {
-      enable: false,
-    },
-    life: {
-      duration: {
-        sync: true,
-        value: 5,
-      },
-      count: 1,
-    },
-    move: {
-      enable: true,
-      gravity: {
-        enable: true,
-        acceleration: 10,
-      },
-      speed: {
-        min: 10,
-        max: 20,
-      },
-      decay: 0.1,
-      direction: "none",
-      straight: false,
-      outModes: {
-        default: "destroy",
-        top: "none",
-      },
-    },
-    rotate: {
-      value: {
-        min: 0,
-        max: 360,
-      },
-      direction: "random",
-      move: true,
-      animation: {
-        enable: true,
-        speed: 30,
-      },
-    },
-    tilt: {
-      direction: "random",
-      enable: true,
-      move: true,
-      value: {
-        min: 0,
-        max: 360,
-      },
-      animation: {
-        enable: true,
-        speed: 60,
-      },
-    },
-    roll: {
-      darken: {
-        enable: true,
-        value: 25,
-      },
-      enable: true,
-      speed: {
-        min: 15,
-        max: 25,
-      },
-    },
-    wobble: {
-      distance: 30,
-      enable: true,
-      move: true,
-      speed: {
-        min: -15,
-        max: 15,
-      },
-    },
-  },
-  emitters: {
-    life: {
-      count: 30,
-      duration: 0.2,
-      delay: 0.3,
-    },
-    rate: {
-      delay: 0.1,
-      quantity: 400,
-    },
-    size: {
-      width: 0,
-      height: 0,
-    },
-  },
-};
---
+++ controllers/lire.js
@@ -0,0 +1,96 @@
+import { options } from "../particles/particles-options.js";
+
+const btnJouerLire = document.querySelector(".btn-lire");
+const nomAleatoire = document.querySelector(".nom-aleatoire");
+const dynamicChiots = document.querySelector(".dynamic-chiots");
+const chiotButtons = document.querySelectorAll(".img-btn");
+const audio = document.querySelector(".audio1");
+const chiots = [
+  "everest",
+  "rocky",
+  "stella",
+  "marcus",
+  "ruben",
+  "zuma",
+  "chase",
+];
+
+let currentChiot = "";
+
+const generateurDeNom = () => {
+  let randomInt = Math.floor(Math.random() * 7);
+
+  currentChiot = chiots[randomInt];
+
+  for (let lettre of currentChiot) {
+    const imgLettre = document.createElement("img");
+    imgLettre.classList.add("img-lettre");
+    imgLettre.src = `../assets/alphabet/${lettre}.png`;
+
+    nomAleatoire.append(imgLettre);
+  }
+};
+
+const afficheImageChiots = (currentChiot) => {
+  let randomChiotPlace = Math.floor(Math.random() * 3);
+
+  chiotButtons.forEach((imgButton, index) => {
+    let randomInt = Math.floor(Math.random() * 7);
+
+    if (index === randomChiotPlace) {
+      imgButton.src = `../assets/${currentChiot}.png`;
+    } else {
+      imgButton.src = `../assets/${chiots[randomInt]}.png`;
+    }
+
+    dynamicChiots.style.display = "flex";
+  });
+};
+
+const verifieChiotClique = (e) => {
+  const chiotCliqueTmp = e.target.src.split("/");
+  const chiotCliqueTmp2 = chiotCliqueTmp[chiotCliqueTmp.length - 1];
+  const chiotClique = chiotCliqueTmp2.split(".")[0];
+
+  if (chiotClique === currentChiot) {
+    audio.src = "../assets/reactions/cris-de-joie.mp3";
+    audio.setAttribute("loop", "");
+
+    tsParticles.load("tsparticles", options);
+
+    setTimeout(() => {
+      audio.pause();
+    }, 6000);
+
+    chiotButtons.forEach((chiotBtn) => {
+      if (chiotBtn !== e.target) {
+        chiotBtn.style.display = "none";
+      } else {
+        chiotBtn.classList.add("img-chiot-trouve");
+        // chiotBtn.classList.remove("img-btn");
+      }
+    });
+
+    // dynamicChiots.style.display = "none";
+  } else {
+    audio.src = `../assets/sons/mauvais.mp3`;
+  }
+};
+
+btnJouerLire.addEventListener("click", () => {
+  chiotButtons.forEach((chiotBtn) => {
+    chiotBtn.classList.remove("img-chiot-trouve");
+    // chiotBtn.classList.add("img-btn");
+    chiotBtn.style.display = "flex";
+  });
+  audio.removeAttribute("loop");
+  nomAleatoire.innerHTML = "";
+  generateurDeNom();
+  afficheImageChiots(currentChiot);
+});
+
+chiotButtons.forEach((imgBtn) => {
+  imgBtn.addEventListener("click", (e) => {
+    verifieChiotClique(e);
+  });
+});
